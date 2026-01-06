b08264016da28d37afa679dd6b3ca397e885ce09
Harry Yep <git@hode.co.uk>
2026-01-06 14:25:44 +0800

    feat: init

---
+++ scode/scodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  scodeApp.swift
+//  scode
+//
+//  Created by Harry Yep on 6/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct scodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
